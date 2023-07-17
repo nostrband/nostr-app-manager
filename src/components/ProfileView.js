7b8f04cef18bc404bd8d6b9ce2d84bd6ca4118d7
Kamchybekk <manasbekoovich@gmail.com>
2023-07-17 09:58:26 +0600

    Showing a spinner while data is loading on the app page

--- src/components/ProfileView.js
+++ src/components/ProfileView.js
@@ -92,75 +92,75 @@ const ProfileView = () => {
   if (!npub) return null;
   return (
     <>
-      {isLoading ? (
-        <div className="d-flex justify-content-center mt-5">
-          <Spinner className="text-primary" />
-        </div>
-      ) : (
-        <>
-          {apps && (
-            <div className="mt-5">
-              <Profile profile={apps.meta} pubkey={pubkey} />
-              <h4 className="mt-5">Published apps:</h4>
-              {!Object.keys(apps.apps).length && 'Nothing yet.'}
-              {apps.apps && (
-                <ListGroup>
-                  {Object.keys(apps.apps).map((name) => {
-                    const app = apps.apps[name];
-                    const h = app.handlers[0];
-                    return <AppSelectItem key={h.name} app={h} />;
-                  })}
-                </ListGroup>
-              )}
-              <div className="mt-2">
-                <Link to={cmn.formatAppEditUrl('')}>
-                  <Button variant="primary">Add app</Button>
-                </Link>
-              </div>
-              <h4 className="mt-5">Used apps:</h4>
-              {!recomms.length && 'Nothing yet.'}
-              {recomms.length > 0 && (
-                <ListGroup>
-                  {reorganizesData.map((group) => (
-                    <>
-                      <div key={group.name} className="mb-3">
-                        {group.apps.map((app) => {
-                          return (
-                            <>
-                              <AppSelectItem
-                                showKinds
-                                selecteAppForEdit={() => {
-                                  setSelectedApp({
-                                    app: { ...app },
-                                    kinds: group.kinds,
-                                  });
-                                  setShowEditModal(app.id);
-                                }}
-                                myApp={myNpubKey === npub}
-                                key={app.id}
-                                app={{ ...app, forKinds: group.kinds }}
-                              />
-                              {app.id === showEditModal ? (
-                                <EditAppModal
-                                  getRecomnsQuery={getRecomnsQuery}
-                                  handleEditClose={handleCloseModal}
-                                  openModal={app.id === showEditModal}
-                                  selectedApp={selectedApp}
-                                  setSelectedApp={setSelectedApp}
-                                />
-                              ) : null}
-                            </>
-                          );
-                        })}
-                      </div>
-                    </>
-                  ))}
-                </ListGroup>
-              )}
-            </div>
+    {isLoading ? 
+      <div className="d-flex justify-content-center mt-5">
+      <Spinner className="text-primary" />
+    </div> :  <> 
+    {apps && (
+        <div className="mt-5">
+          <Profile profile={apps.meta} pubkey={pubkey} />
+          <h4 className="mt-5">Published apps:</h4>
+          {!Object.keys(apps.apps).length && 'Nothing yet.'}
+          {apps.apps && (
+            <ListGroup>
+              {Object.keys(apps.apps).map((name) => {
+                const app = apps.apps[name];
+                const h = app.handlers[0];
+                return <AppSelectItem key={h.name} app={h} />;
+              })}
+            </ListGroup>
           )}
-        </>
+          <div className="mt-2">
+            <Link to={cmn.formatAppEditUrl('')}>
+              <Button variant="primary">Add app</Button>
+            </Link>
+          </div>
+          <h4 className="mt-5">Used apps:</h4>
+          {!recomms.length && 'Nothing yet.'}
+          {recomms.length > 0 && (
+            <ListGroup>
+              {reorganizesData.map((group) => (
+                <>
+                  <div key={group.name} className="mb-3">
+                    {group.apps.map((app) => {
+                      return (
+                        <>
+                          <AppSelectItem
+                            showKinds
+                            selecteAppForEdit={() => {
+                              setSelectedApp({
+                                app: { ...app },
+                                kinds: group.kinds,
+                              });
+                              setShowEditModal(app.id);
+                            }}
+                            myApp={myNpubKey === npub}
+                            key={app.id}
+                            app={{ ...app, forKinds: group.kinds }}
+                          />
+                          {app.id === showEditModal ? (
+                            <EditAppModal
+                              getRecomnsQuery={getRecomnsQuery}
+                              handleEditClose={handleCloseModal}
+                              openModal={app.id === showEditModal}
+                              selectedApp={selectedApp}
+                              setSelectedApp={setSelectedApp}
+                            />
+                          ) : null}
+                        </>
+                      );
+                    })}
+                  </div>
+                </>
+              ))}
+            </ListGroup>
+          )}
+        </div>
       )}
+    </>
+  }
+    
+     
     </>
   );
 };
