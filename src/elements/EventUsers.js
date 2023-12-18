cfe4c8f2330b03d3a9f19f20591bc797b76fd0cd
Kamchybekk <manasbekoovich@gmail.com>
2023-12-18 13:27:47 +0600

    Added detailed information and date in EventUsers.

--- src/elements/EventUsers.js
+++ src/elements/EventUsers.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 import Profile from './Profile';
 import * as cmn from '../common';
+import { nip19 } from '@nostrband/nostr-tools';
 
 const EventUsers = ({ event, users }) => {
+  const [showDetails, setShowDetails] = useState(false);
   let titleEvent = event?.tags
     ?.filter((tag) => tag[0] === 'name')
     ?.map((tag) => tag[1])[0];
 
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
-    <div>
+    <>
       <h3>{titleEvent}</h3>
-      <ListGroup>
+      <ListGroup className="mb-1">
         {users?.users?.map((user) => {
           let profile = user?.content
             ? cmn.convertContentToProfile([user])
@@ -29,11 +35,42 @@ const EventUsers = ({ event, users }) => {
         })}
       </ListGroup>
       {users.countOfOtherUsers > 0 ? (
-        <span className="mx-1 mt-2">
+        <span>
           And <strong>{users.countOfOtherUsers}</strong> more profiles
         </span>
       ) : null}
-    </div>
+      <Col className="mt-1" xs={12}>
+        <small className="text-muted">
+          {new Date(event.created_at * 1000).toLocaleString()}
+          <span
+            onClick={toggleDetails}
+            className="ms-2 fs-7 pointer"
+            style={{ textDecoration: 'underline' }}
+          >
+            {showDetails ? 'Less info' : 'More info'}
+          </span>
+        </small>
+      </Col>
+
+      {showDetails && (
+        <Col xs={12}>
+          <small className="text-muted">
+            <div
+              style={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              Id: <b>{nip19.noteEncode(event.id)}</b>
+            </div>
+            <div>
+              Kind: <b>{event.kind}</b>
+            </div>
+          </small>
+        </Col>
+      )}
+    </>
   );
 };
 
