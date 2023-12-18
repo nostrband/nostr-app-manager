2477553faab4aa3982182698e4c0d1a757d341ee
Kamchybekk <manasbekoovich@gmail.com>
2023-12-18 21:42:04 +0600

    save

--- src/elements/EventNote.js
+++ src/elements/EventNote.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Profile from './Profile';
+import * as cmn from '../common';
 
 const EventNote = (props) => {
   const event = props.event;
@@ -13,7 +14,12 @@ const EventNote = (props) => {
         <Col xs={12}>{event.content}</Col>
         <Col xs={12}>
           <small className="text-muted">
-            {new Date(event.created_at * 1000).toLocaleString()}
+            {cmn.getKindLabel(event.kind)
+              ? cmn.capitalizeFirstLetter(cmn.getKindLabel(event.kind))
+              : event.kind}
+            <span className="mx-2">
+              {new Date(event.created_at * 1000).toLocaleString()}
+            </span>
           </small>
         </Col>
       </Row>
