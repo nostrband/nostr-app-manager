85de29a7c27b9cabba2677c599a84903b6d4d6fd
Kamchybekk <manasbekoovich@gmail.com>
2023-12-19 12:46:48 +0600

    remove link

--- src/components/EventApps/EventTags.js
+++ src/components/EventApps/EventTags.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const EventTags = ({ tags }) => {
-  const navigate = useNavigate();
   return (
     <div className="d-flex flex-wrap">
       <strong>Tags:</strong>
       {tags.map((t) => {
         return (
-          <span
-            class="pointer tag-event-apps mx-1"
-            onClick={() => navigate(`/tag/${t}`)}
-            key={t}
-          >
+          <span class="pointer tag-event-apps mx-1" key={t}>
             #{t}
           </span>
         );
